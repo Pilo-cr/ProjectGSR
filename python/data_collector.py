757fbabe7a6a74676777b60701ab3d3c167887f5
Pilo-cr <123471922+Pilo-cr@users.noreply.github.com>
2024-03-07 20:20:00 +0000

    removed copy of data collector

--- python/data_collector.py
+++ python/data_collector.py
@@ -1,21 +1,34 @@
 import serial
+import matplotlib.pyplot as plt
 
 # TODO: This should be a program argument
 serial_port = "/dev/cu.usbmodem1101"
-data_file_location = "dataSamples/data.txt"
+data_file_location = "dataSamples/data.csv"
 
 try:
     # Connect to the serial port where Arduino is streaming data
     arduino = serial.Serial(serial_port, timeout = 1)
 
+    x = []
+    y = []
+    plt.ion()
+    plt.title('Realtime Stress', fontsize = 20)
+    plt.show()
+
     with open(data_file_location, "w") as f:
         while True: # This loop will run until press Ctrl+C
-            streamed_line = arduino.readline()
-            f.write(streamed_line.decode())
-            f.flush()
+            streamed_line = arduino.readline().decode()
+            if len(streamed_line) > 0:
+                f.write(streamed_line)
+                f.flush()
+
+                data_splitted = streamed_line.split(",")
+                y.append(int(data_splitted[0]))
+                x.append(int(data_splitted[1]))
+                plt.plot(x, y, color = 'g', linestyle = 'solid', label = "stress data")
+                plt.draw()
+                plt.pause(0.1)
 
 except Exception as e:
     print("error: " , e)
-
-
 
